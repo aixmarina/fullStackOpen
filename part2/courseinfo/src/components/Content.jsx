232f2aa26a3d6c8f2483ddf727da6846cde3289f
marina aix <marina.aix.gomez@gmail.com>
2023-09-07 16:38:56 +0200

    finish exercise 2.2

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -1,4 +1,5 @@
 import Part from "./Part"
+import Total from "./Total"
 
 const Content = ({ parts }) => {
 	return(
@@ -6,6 +7,7 @@ const Content = ({ parts }) => {
          <Part part={parts[0]} />
          <Part part={parts[1]} />
          <Part part={parts[2]} />
+         <Total parts={parts}/>
 		</div>
 )}
 
---
+++ part2/courseinfo/src/components/Total.jsx
@@ -0,0 +1,8 @@
+const Total = ({ parts }) => {
+	return(
+		<b>
+			total of {parts[0].exercises + parts[1].exercises + parts[2].exercises} exercises
+		</b>
+)}
+
+export default Total 
