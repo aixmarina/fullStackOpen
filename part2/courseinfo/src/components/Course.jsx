70f89d4b31c734c211c938b6e3c46eff57c45b52
marina aix <marina.aix.gomez@gmail.com>
2023-09-07 16:31:29 +0200

    finish exercise 2.1

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,12 @@
+import Content from "./Content"
+import Header from "./Header"
+
+const Course = ({ course }) => {
+	return(
+		<div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+		</div>
+)}
+
+export default Course 
