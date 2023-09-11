2caf21c8d9f54700de630bd17725ad8d84e8b83c
marina aix <marina.aix.gomez@gmail.com>
2023-09-11 21:00:40 +0200

    finish exercise 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -16,7 +16,12 @@ const App = () => {
       name: newName, 
     }
 
-    setPersons(persons.concat(personObject))
+    if (persons.some(person => JSON.stringify(person) === JSON.stringify(personObject))) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat(personObject))
+    }
+    
     setNewName('')
   }
 
